ca0cd329bc5ef82f37f036f0ad6293c0453e2e8a
Hiro <hirotaka.takahashi@widsley.com>
2013-05-03 19:44:44 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 SampleProject::Application.routes.draw do
+#  get "static_pages/..."
+
+  get "static_pages/home"
+  get "static_pages/help"
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
