3d0de3e5ef2c1925cb4af9d99c55bcf6e55f3209
Hiro <hirotaka.takahashi@widsley.com>
2013-05-15 22:56:00 +0900

    make a basic user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe "StaticPages" do
 	subject { page }
 
+	shared_examplees_for "all static pages" do
+		it { should have_selector('h1', test: heading)}
+		it { should have_selector('title', text: full_title(page_title))}
+	end
+
 	describe "Home page" do
 		before { visit root_path }
+		let(:heading) {'Sample App'}
+		let(:page_title) {''}
 
-		it { should have_selector('h1', text: 'Sample App') }
-		it { should have_selector('title', text: full_title('')) }
+		it_should_behave_like "all static pages"
 		it { should_not have_selector 'title', text: '| Home' }
 	end
 
@@ -32,5 +38,18 @@ describe "StaticPages" do
      	it { should have_selector('title', text: full_title('Contact')) }
 	end
 
-
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About Us')
+		click_link "Help"
+		page.should
+		click_link "Contact"
+		page.should
+		click_link "Home"
+		click_link "Sign up now!"
+		page.should
+		click_link "sample app"
+		page.should
+	end
 end
