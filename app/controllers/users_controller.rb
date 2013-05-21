6e64f6afbded35e43201b3481bb7527e47ea3fe8
Hiro <hirotaka.takahashi@widsley.com>
2013-05-21 19:04:05 +0900

    Add user micropots

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -62,4 +63,5 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
+    
 end
